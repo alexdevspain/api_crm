d31b754e2359e0ffa0ead2fbdb579e7a8101df82
Alo <lopezortegadev@gmail.com>
2022-09-26 16:56:27 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.jsx"],
+  /**Aqui si utilizas typescript es .tsx o ts" */
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
